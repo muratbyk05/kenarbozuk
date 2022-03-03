5a280f54f16e6e4bdee91296b13f6e8e8faf4acb
muratbyk05 <95565340+muratbyk05@users.noreply.github.com>
2022-03-03 11:53:20 +0300

    Add files via upload

---
+++ kenar/main.js
@@ -0,0 +1,8 @@
+function hesap(){
+    let kkkenar=document.getElementById("kkenar")
+    let uukenar=document.getElementById("ukenar")
+    let sonuç=document.getElementById("sonuc")
+
+    let hesala=(kkkenar*1*uukenar*1)
+    sonuç.innerHTML="sonuc"+hesala
+}
